f898be49bf5d839326cc632c5cb42b80910c36f5
z1mo0x <droweckiy@mail.ru>
2026-01-10 17:06:02 +0300

    second

--- src/components/ux/portfolio/portfolio-item.tsx
+++ src/components/ux/portfolio/portfolio-item.tsx
@@ -14,11 +14,11 @@ export default function PortfolioItem({ image, index }: Props) {
             initial={{ opacity: 0, scale: .5 }}
             whileInView={{ opacity: 1, scale: 1 }}
             transition={{ duration: .5 }}
-            className='group  relative h-100 w-full overflow-hidden rounded-lg object-cover'>
+            className='group cursor-pointer relative h-100 w-full overflow-hidden rounded-lg object-cover'>
             <Image src={image} alt={`Портфолио | рис. ${index}`} />
             <div className="
              group-hover:translate-x-0  
-             z-5 duration-500 absolute h-full top-0 left-0 w-full -translate-x-full bg-bardo p-5">
+             z-5 duration-500 absolute rounded-lg h-full top-0 left-0 w-full -translate-x-full bg-bardo p-5">
                 <div className="font-bold text-white">
                     Какие услуги были оказаны:
                 </div>
